a1f3f3fd515a24d333f55287b58e58628676c0e1
Frederik Hoffmann <frederik.hoffmann99@gmail.com>
2019-11-11 19:29:26 +0100

    Add save method and throw exceptions instead of catching them

--- src/main/java/de/yannikpfeiffer/hadrysconverter/optionloading/OptionsLoader.java
+++ src/main/java/de/yannikpfeiffer/hadrysconverter/optionloading/OptionsLoader.java
@@ -8,26 +8,32 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class OptionsLoader {
-    Options options;
+    private Options options;
+    private File settingsFile;
 
     public OptionsLoader() {
         this.options = new Options();
+        settingsFile = Path.of(System.getProperty("user.home")).resolve(".hadrysConverter-settings.json").toFile();
     }
 
-    public void loadOptions() {
-        Path home = Path.of(System.getProperty("user.home"));
-        File settingsFile = home.resolve(".hadrysConverter-settings.json").toFile();
-        try {
+    public Options getOptions() {
+        return options;
+    }
+
+    public void loadOptions() throws IOException {
+        if (settingsFile.createNewFile()) {
+            saveOptions(new Options());
+        } else {
             ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-            if (settingsFile.createNewFile()) {
-                objectMapper.writeValue(settingsFile, new Options());
-            } else {
-                options = objectMapper.readValue(settingsFile, Options.class);
-                System.out.println(options);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            options = objectMapper.readValue(settingsFile, Options.class);
+            System.out.println(options);
         }
     }
+
+    public void saveOptions(Options options) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.writeValue(settingsFile, options);
+    }
+
 }
